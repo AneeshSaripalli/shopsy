62f10141a1e523bd5cbba497657276241084ec95
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2020-07-04 09:30:57 -0500

    Synchonized rust kafka consumer & producer.

---
+++ rust-kafka-consumer/src/main.rs
@@ -0,0 +1,25 @@
+use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
+
+fn main() {
+    let mut consumer = Consumer::from_hosts(vec!["kafka:9092".to_owned()])
+        .with_topic_partitions("KafkaConnectivityTest".to_owned(), &[0])
+        .with_fallback_offset(FetchOffset::Latest)
+        .with_offset_storage(GroupOffsetStorage::Kafka)
+        .create()
+        .unwrap();
+
+    loop {
+        for msg_set in consumer.poll().unwrap().iter() {
+            for m in msg_set.messages() {
+                println!("{:?}", m);
+            }
+
+            let result = consumer.consume_messageset(msg_set);
+            if let Ok(_) = result {
+                println!("Successfully consumed messages.");
+            }
+        }
+
+        let commit = consumer.commit_consumed();
+    }
+}
