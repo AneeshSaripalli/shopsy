717216632b29d125c84ca85d0a6caef0e87f4c4d
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2020-07-04 15:54:55 -0500

    Handle Kafka message callback in Rust.

--- rust-kafka-consumer/src/main.rs
+++ rust-kafka-consumer/src/main.rs
@@ -1,5 +1,11 @@
 use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
 
+fn handle_kafka_message(message: &kafka::consumer::Message<'_>) {
+    let kafka_msg_value = std::str::from_utf8(message.value).unwrap();
+
+    println!("Kafka value {:?}", kafka_msg_value);
+}
+
 fn main() {
     let mut consumer = Consumer::from_hosts(vec!["kafka:9092".to_owned()])
         .with_topic_partitions("KafkaConnectivityTest".to_owned(), &[0])
@@ -10,9 +16,8 @@ fn main() {
 
     loop {
         for msg_set in consumer.poll().unwrap().iter() {
-            println!("Message in poll!");
             for m in msg_set.messages() {
-                println!("{:?}", m);
+                handle_kafka_message(m);
             }
 
             let result = consumer.consume_messageset(msg_set);
@@ -21,8 +26,6 @@ fn main() {
             }
         }
 
-        let commit = consumer.commit_consumed();
-
-        println!("Looping");
+        consumer.commit_consumed();
     }
 }
