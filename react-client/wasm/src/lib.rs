8f272ce937e47f0fdb4f382544c74dad3c79eae9
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2020-07-05 01:32:58 -0500

    Set up react client with WASM integration & Docker build process.

---
+++ react-client/wasm/src/lib.rs
@@ -0,0 +1,17 @@
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> i32 {
+    return a + b;
+}
+
+#[wasm_bindgen]
+pub fn fib(n: i32) -> i32 {
+    if n == 0 || n == 1 {
+        return n;
+    }
+
+    fib(n - 1) + fib(n - 2)
+}
