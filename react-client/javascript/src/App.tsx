720e4fadd5570a1afa977ad22572b2ba51bd4724
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2020-07-07 10:11:53 -0500

    Setup Github Packge (typescript, Commonjs) for Protos library.

--- react-client/javascript/src/App.tsx
+++ react-client/javascript/src/App.tsx
@@ -3,5 +3,14 @@ import React from 'react';
 import './App.css';
 import AppRouter from './router/AppRouter';
 
+import { api } from "protos";
+
+const x = api.Person.create({
+    email: "aneeshsaripalli@gmail.com",
+    id: 1234,
+    name: "Aneesh Saripalli"
+})
+
+console.log(x);
 
 export default () => <AppRouter />
